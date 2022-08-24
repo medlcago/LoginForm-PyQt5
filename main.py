36ea4719372631719237ec462f04b07e15ceacf8
Alexander <87269586+xMedicago@users.noreply.github.com>
2022-08-24 19:28:30 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
-from PyQt5 import QtCore
-from PyQt5.uic import loadUi
+import webbrowser
 import images
+
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QWidget, QApplication, QMessageBox
+from PyQt5.QtGui import QIcon
+from PyQt5.uic import loadUi
 
 
 class Login(QWidget):
@@ -12,6 +15,12 @@ class Login(QWidget):
 
         self.old_pos = None
         loadUi(".\\uiFiles\\LoginUi.ui", self)
+        self.input_login.setMaxLength(64)
+        self.input_password.setMaxLength(64)
+        self.get_buttons()
+
+    def get_buttons(self):
+        self.GitHub.clicked.connect(lambda: webbrowser.open("https://github.com/xMedicago"))
 
     # вызывается при нажатии кнопки мыши
     def mousePressEvent(self, event):
@@ -29,6 +38,23 @@ class Login(QWidget):
             return
         delta = event.pos() - self.old_pos
         self.move(self.pos() + delta)
+
+    #  вызывается при закрытии приложения
+    def closeEvent(self, event):
+        msg = QMessageBox(self)
+        msg.setWindowIcon(QIcon(".\\images\\icon.png"))
+        msg.setWindowTitle("Уже уходите?)")
+        msg.setIcon(QMessageBox.Question)
+        msg.setText("Вы уверены, что хотите закрыть приложение?")
+        okButton = msg.addButton("Yes", QMessageBox.AcceptRole)
+        msg.addButton("No", QMessageBox.RejectRole)
+
+        msg.exec_()
+
+        if msg.clickedButton() == okButton:
+            event.accept()
+        else:
+            event.ignore()
 
 
 if __name__ == '__main__':
